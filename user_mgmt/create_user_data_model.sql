af98157e1145141f327d8f7722ea280bd25bf26d
rstrobel <ronald.strobel@gmail.com>
2016-09-10 10:33:14 +0200

    new code

--- user_mgmt/create_user_data_model.sql
+++ user_mgmt/create_user_data_model.sql
@@ -4,8 +4,9 @@ CREATE TABLE haushalt.tb_users
 (
   usr_id serial PRIMARY KEY,
   usr_name varchar(30) NOT NULL,
-  usr_role_id integer
+  usr_role_id integer,
+  usr_status char(1) NOT NULL
 );
 
-INSERT INTO haushalt.tb_users VALUES (DEFAULT, 'Ronald', 1);
-INSERT INTO haushalt.tb_users VALUES (DEFAULT, 'Simone', 1);
+INSERT INTO haushalt.tb_users VALUES (DEFAULT, 'Ronald', 1, 'A';
+INSERT INTO haushalt.tb_users VALUES (DEFAULT, 'Simone', 1, 'A');
--- user_mgmt/haushalt_user_functions.py
+++ user_mgmt/haushalt_user_functions.py
@@ -8,7 +8,8 @@
 #
 #########################################################
 
-
+def user_mgmt_interface():
+    # filter, show, create and update user data, delete
 
 
 #########################################################
@@ -18,6 +19,17 @@
 #########################################################
 
 
+def create_user ():
+
+def remove_user ():
+    # only the ones with 0 operations
+
+def modify_user ():
+
+def
+
+def list_all_users ():
+
 
 #########################################################
 #
@@ -25,4 +37,10 @@
 #
 #########################################################
 
-def insert_user (l_user_record):
+def insert_new_user_record (l_user_record):
+
+def update_user_record (l_user_record):
+
+def delete_user_record (l_user_record):
+
+def retrieve_user_record (usr_name):
